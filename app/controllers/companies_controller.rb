c0f390eb3e91aa9fed7181138742d7a13536b6df
MaryAlice Peng <maryalice.peng@gmail.com>
2025-02-26 22:28:53 +0000

    Most of class

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,24 +2,33 @@ class CompaniesController < ApplicationController
 
   def index
     # find all Company rows
+    @companies = Company.all
     # render companies/index view
   end
 
   def show
-    # find a Company
-    # render companies/show view with details about Company
+  # find company 
+  @company = Company.find_by({"id" => params["id"]})
+  @contacts = Contact.where({"company_id" => @company["id']"]})
+
   end
 
   def new
     # render view with new Company form
   end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+   # find a Company
+   @company = Company.find_by({"id" => params["id"]})
+   # render companies/show view with details about Company
+   @company["name"] = params["name"]
+   @company["city"] = params["city"]
+   @company["state"] = params["state"]
+   #save the new Company row
+   @company.save
+   #redirect user 
+   redirect_to "/companies"
+  end
 
   # def edit
   #   # find a Company
